a4872049f10c0e0be5cb606e0d7a19c53670ada2
Kateryna <kati.skrinnikova@gmail.com>
2024-09-16 16:23:31 +0300

    header fixed

---
+++ src/js/header-fixed.js
@@ -0,0 +1,14 @@
+const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
+console.log(headerHeight);
+const main = document.querySelector('main');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > headerHeight) {
+    header.classList.add('fixed');
+    main.style.paddingTop = `${headerHeight}px`;
+  } else {
+    header.classList.remove('fixed');
+    main.style.paddingTop = 0;
+  }
+});
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
 import './js/swiper.js';
-
-
-
 import './js/mobile-menu';
+import './js/header-fixed.js';
