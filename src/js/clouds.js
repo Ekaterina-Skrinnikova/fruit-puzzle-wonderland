9d38e9816ef756304f339cbd493441d8e5deeadb
Kateryna <kati.skrinnikova@gmail.com>
2024-09-17 20:25:09 +0300

    added animation in hero

--- src/js/clouds.js
+++ src/js/clouds.js
@@ -7,3 +7,16 @@
 //     }, index * 500);
 //   });
 // });
+
+window.addEventListener('load', function () {
+  const clouds = document.querySelectorAll('.cloud');
+  clouds.forEach((cloud, index) => {
+    if (index === 1) {
+      cloud.classList.add('animation-1');
+    } else if (index === 0) {
+      cloud.classList.add('animation-2');
+    } else if (index === 3) {
+      cloud.classList.add('animation-3');
+    }
+  });
+});
--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import './js/swiper.js';
 import './js/mobile-menu';
 import './js/header-fixed.js';
 import './js/transition-to-link.js';
+import './js/clouds.js';
