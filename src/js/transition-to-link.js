1774975a1a31495c5a8edc88b0bad71bfa15393a
Kateryna <kati.skrinnikova@gmail.com>
2024-09-17 11:09:20 +0300

    added js to link

--- src/js/transition-to-link.js
+++ src/js/transition-to-link.js
@@ -1,6 +1,5 @@
 const header = document.querySelector('header');
 const headerHeight = header.offsetHeight;
-console.log(headerHeight);
 
 document.querySelectorAll('.header-nav-link').forEach(item => {
   item.addEventListener('click', e => {
@@ -9,14 +8,21 @@ document.querySelectorAll('.header-nav-link').forEach(item => {
     const sectionId = item.getAttribute('href').substring(1);
     const targetSection = document.getElementById(sectionId);
 
-    if (
-      Array.from(document.querySelectorAll('section')).indexOf(targetSection) %
-        2 ===
-      1
-    ) {
-      targetSection.style.paddingTop = `${headerHeight}px`;
-      console.log(headerHeight);
-    }
+    targetSection.style.paddingTop = `${headerHeight}px`;
+
+    window.matchMedia('(min-width: 1440px)').addEventListener('change', () => {
+      if (window.matchMedia('(min-width: 1440px)').matches) {
+        if (
+          Array.from(document.querySelectorAll('section')).indexOf(
+            targetSection
+          ) %
+            2 ===
+          1
+        ) {
+          targetSection.style.paddingTop = `${headerHeight}px`;
+        }
+      }
+    });
 
     targetSection.scrollIntoView({
       behavior: 'smooth',
